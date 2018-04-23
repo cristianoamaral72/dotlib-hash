97067930d463e02a05632da36c9007b9fda0d77a
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-23 10:44:38 -0300

    minor

--- Dotlib.Hash/Md5.cs
+++ Dotlib.Hash/Md5.cs
@@ -16,18 +16,16 @@ namespace Dotlib.Hash
         /// <param name="input">String a ser calculada</param>
         new public static string Generate(string input)
         {
-            MD5 md5Hash = MD5.Create();
-
             // Convertendo a String para array de bytes
-            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
 
             // StringBuilder para recompor a string
             var builder = new StringBuilder();
 
             // Formatando cada byte como uma String em hexadecimal
-            for (int i = 0; i < data.Length; i++)
+            for (int i = 0; i < hash.Length; i++)
             {
-                builder.Append(data[i].ToString("x2"));
+                builder.Append(hash[i].ToString("x2"));
             }
 
             return builder.ToString();
